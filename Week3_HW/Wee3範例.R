a683d85475bbdd04e064cdc97309312a6c34abce
Lin-Sia <55202692+Lin-Sia@users.noreply.github.com>
2020-10-29 10:37:15 +0800

    完成範例

--- Week3_HW/Wee3範例.R
+++ Week3_HW/Wee3範例.R
@@ -1,2 +1,17 @@
 library(tidyverse)
-reader('TWSE_Stock Data_2012-2017.csv')
+Data <- read_csv('TWSE_Stock Data_2012-2017.csv')
+library(tidyverse)
+#前述步驟讀資料
+Stock_Price <- as_tibble(read_csv("TWSE_Stock Data_2012-2017.csv", col_types = cols(security_id = col_character()) ))
+
+#gather 函數
+Stock_Price_colname <- colnames(Stock_Price)
+A <- Stock_Price %>% gather(key = "time", value = "price", Stock_Price_colname[3:ncol(Stock_Price)])
+
+#spread函數
+Stock_Price <- A %>% spread(type, price)
+
+#separate函數
+Stock_Price$time <- as.Date(Stock_Price$time, format = "%Y/%m/%d")
+Stock_Price %>% separate(col = time, into = c("year", "month", "day"), sep = "-", convert = T)
+
